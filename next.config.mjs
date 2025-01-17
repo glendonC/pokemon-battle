fb3b678fe6b69d296dcd04092b96178ba9f133e6
Glendon Chin <glendonchin@gmail.com>
2025-01-16 19:50:11 -0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-    webpack: (config) => {
-      config.externals = config.externals || [];
-      config.externals.push({
-        canvas: 'canvas',
-        'utf-8-validate': 'utf-8-validate',
-        'bufferutil': 'bufferutil',
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei', '@babylonjs/core', '@babylonjs/loaders'],
+  webpack: (config) => {
+    config.externals = config.externals || [];
+    config.externals.push({
+      canvas: 'canvas',
+      'utf-8-validate': 'utf-8-validate',
+      'bufferutil': 'bufferutil',
+    });
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
+
